400f0dd7096744fe057937dbd724c6ca2c29de9c
Matheuschmidt <matheuschmidt.dev@gmail.com>
2026-08-26 22:42:19 -0300

    feat: add exercises consuming external APIs

---
+++ module 4/src/MainCoinGecko.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class MainCoinGecko {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escolha uma criptomoeda para busca: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://api.coingecko.com/api/v3/simple/price?vs_currencies=brl&ids=" + busca + "&x_cg_demo_api_key=CG-8csEWY7bZcW2hXhZBqGqVytF";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
---
+++ module 4/src/MainGoogleBooks.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class MainGoogleBooks {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Digite um nome de um livro para busca: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://openlibrary.org/search.json?q=" + busca;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
